4f659f29c7cfeeb31907ec7c35858c368b65ee76
bohdan_peliutkevich <bohdanpeliutkevich@gmail.com>
2023-12-10 23:19:28 +0300

    Jenkins parameters

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -15,13 +15,13 @@ import static com.codeborne.selenide.Selenide.closeWebDriver;
 public class TestBase {
     @BeforeAll
     static void beforeFillForm(){
-        Configuration.browser = System.getProperty("browser", "chrome");
-        Configuration.browserSize =System.getProperty("browserSize","1920x1080");
-        Configuration.browserVersion = System.getProperty(" browserVersion", "100.0");
-        Configuration.pageLoadStrategy = "eager";
         Configuration.baseUrl= System.getProperty("baseUrl", "https://demoqa.com");
-        Configuration.holdBrowserOpen = false;
+        Configuration.browser = System.getProperty("browser", "chrome");
+        Configuration.browserVersion = System.getProperty("browserVersion", "100.0");
+        Configuration.browserSize =System.getProperty("browserSize","1920x1080");
         Configuration.remote = System.getProperty("selenoid", "https://user1:1234@selenoid.autotests.cloud/wd/hub");
+        Configuration.pageLoadStrategy = "eager";
+        Configuration.holdBrowserOpen = false;
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
