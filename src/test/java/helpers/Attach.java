0ea50c16d72257b436e81250d00a2f71a78d83d5
bohdan_peliutkevich <bohdanpeliutkevich@gmail.com>
2023-12-10 22:36:36 +0300

    Jenkins parameters

--- src/test/java/helpers/Attach.java
+++ src/test/java/helpers/Attach.java
@@ -44,8 +44,8 @@ public class Attach {
     }
 
     public static URL getVideoUrl() {
-        String selenoidUrl = System.getProperty("selenoidURL","https://selenoid.autotests.cloud/video/");
-        String videoUrl = selenoidUrl + sessionId() + ".mp4";
+//        String selenoidUrl = System.getProperty("selenoidURL","https://selenoid.autotests.cloud/video/");
+        String videoUrl = "https://selenoid.autotests.cloud/video/" + sessionId() + ".mp4";
         try {
             return new URL(videoUrl);
         } catch (MalformedURLException e) {
